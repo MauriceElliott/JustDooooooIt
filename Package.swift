87b011005b29da4cfa50c27df56e0506bc376dab
Maurice Elliott <mauricemelliott@gmail.com>
2025-10-13 12:20:09 +0100

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "JustDooooooIt",
+    products: [
+        .executable(name: "JustDooooooIt", targets: ["JustDooooooIt"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "JustDooooooIt",
+            dependencies: []),
+        .testTarget(
+            name: "JustDooooooItTests",
+            dependencies: ["JustDooooooIt"]),
+    ]
+)
